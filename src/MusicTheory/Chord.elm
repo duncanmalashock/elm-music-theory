13f95da61ededb6c0f8d3992ad2e517e4ee827f4
Duncan Malashock <duncanmalashock@gmail.com>
2020-01-27 23:47:58 -0500

    implement Chord module

---
+++ src/MusicTheory/Chord.elm
@@ -0,0 +1,23 @@
+module MusicTheory.Chord exposing (chord, toPitchClasses)
+
+import MusicTheory.ChordClass as ChordClass
+import MusicTheory.Internal.PitchClass
+import MusicTheory.PitchClass as PitchClass
+
+
+type Chord
+    = Chord PitchClass.PitchClass ChordClass.ChordClass
+
+
+chord : PitchClass.PitchClass -> ChordClass.ChordClass -> Chord
+chord root chordClass =
+    Chord root chordClass
+
+
+toPitchClasses : Chord -> List PitchClass.PitchClass
+toPitchClasses (Chord root chordClass) =
+    List.map
+        (\interval ->
+            MusicTheory.Internal.PitchClass.transposeUp interval root
+        )
+        (ChordClass.toIntervals chordClass)
