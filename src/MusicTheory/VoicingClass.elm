4f524eb5c63bba2699654c34a743da442563c7bb
Duncan Malashock <duncanmalashock@gmail.com>
2020-06-28 11:08:56 -0400

    Implement VoicingClass.builder and related helpers

---
+++ src/MusicTheory/VoicingClass.elm
@@ -0,0 +1,155 @@
+module MusicTheory.VoicingClass exposing
+    ( builder
+    , execute
+    , withFactor
+    , withFactorFrom
+    , withThreeFactorsFrom
+    , withTwoFactorsFrom
+    )
+
+-- Building VoicingClasses
+
+import List.Extra
+import MusicTheory.Interval as Interval
+
+
+type VoicingClassBuilder a
+    = VoicingClassBuilder (List ( a, List Interval.Interval ))
+
+
+builder : (a -> b) -> VoicingClassBuilder (a -> b)
+builder construct =
+    VoicingClassBuilder [ ( construct, [] ) ]
+
+
+withFactor : Interval.Interval -> { mustBeUnique : Bool } -> VoicingClassBuilder (Interval.Interval -> a) -> VoicingClassBuilder a
+withFactor newInterval { mustBeUnique } (VoicingClassBuilder incompleteVoicingsWithIntervalsSoFar) =
+    let
+        addIntervalToVoicing :
+            Interval.Interval
+            -> ( Interval.Interval -> a, List Interval.Interval )
+            -> Maybe ( a, List Interval.Interval )
+        addIntervalToVoicing intervalToApply ( theVoicing, intervalsUsed ) =
+            Just ( theVoicing intervalToApply, intervalToApply :: intervalsUsed )
+    in
+    VoicingClassBuilder
+        (List.filterMap
+            (\voicingWithIntervals ->
+                addIntervalToVoicing newInterval voicingWithIntervals
+            )
+            incompleteVoicingsWithIntervalsSoFar
+        )
+
+
+withFactorFrom : List Interval.Interval -> { mustBeUnique : Bool } -> VoicingClassBuilder (Interval.Interval -> a) -> VoicingClassBuilder a
+withFactorFrom options { mustBeUnique } (VoicingClassBuilder incompleteVoicingsWithIntervalsSoFar) =
+    let
+        addIntervalToVoicing :
+            Interval.Interval
+            -> ( Interval.Interval -> a, List Interval.Interval )
+            -> Maybe ( a, List Interval.Interval )
+        addIntervalToVoicing intervalToApply ( theVoicing, intervalsUsed ) =
+            if mustBeUnique && List.member intervalToApply intervalsUsed then
+                Nothing
+
+            else
+                Just ( theVoicing intervalToApply, intervalToApply :: intervalsUsed )
+    in
+    VoicingClassBuilder
+        (List.concatMap
+            (\voicingWithIntervals ->
+                List.filterMap
+                    (\interval ->
+                        addIntervalToVoicing interval voicingWithIntervals
+                    )
+                    options
+            )
+            incompleteVoicingsWithIntervalsSoFar
+        )
+
+
+withTwoFactorsFrom : List Interval.Interval -> { mustBeUnique : Bool } -> VoicingClassBuilder (Interval.Interval -> Interval.Interval -> a) -> VoicingClassBuilder a
+withTwoFactorsFrom options { mustBeUnique } (VoicingClassBuilder incompleteVoicingsWithIntervalsSoFar) =
+    let
+        addIntervalsToVoicing :
+            ( Interval.Interval, Interval.Interval )
+            -> ( Interval.Interval -> Interval.Interval -> a, List Interval.Interval )
+            -> Maybe ( a, List Interval.Interval )
+        addIntervalsToVoicing ( interval1, interval2 ) ( theVoicing, intervalsUsed ) =
+            if mustBeUnique && List.any (\i -> List.member i intervalsUsed) [ interval1, interval2 ] then
+                Nothing
+
+            else
+                Just ( theVoicing interval1 interval2, [ interval1, interval2 ] ++ intervalsUsed )
+
+        pairs : List Interval.Interval -> List ( Interval.Interval, Interval.Interval )
+        pairs theList =
+            List.Extra.permutations theList
+                |> List.map
+                    (\permutation ->
+                        case permutation of
+                            a :: b :: _ ->
+                                Just ( a, b )
+
+                            _ ->
+                                Nothing
+                    )
+                |> List.filterMap identity
+    in
+    VoicingClassBuilder
+        (List.concatMap
+            (\voicingWithIntervals ->
+                List.filterMap
+                    (\twoIntervals ->
+                        addIntervalsToVoicing twoIntervals voicingWithIntervals
+                    )
+                    (pairs options)
+            )
+            incompleteVoicingsWithIntervalsSoFar
+        )
+
+
+withThreeFactorsFrom : List Interval.Interval -> { mustBeUnique : Bool } -> VoicingClassBuilder (Interval.Interval -> Interval.Interval -> Interval.Interval -> a) -> VoicingClassBuilder a
+withThreeFactorsFrom options { mustBeUnique } (VoicingClassBuilder incompleteVoicingsWithIntervalsSoFar) =
+    let
+        addIntervalsToVoicing :
+            ( Interval.Interval, Interval.Interval, Interval.Interval )
+            -> ( Interval.Interval -> Interval.Interval -> Interval.Interval -> a, List Interval.Interval )
+            -> Maybe ( a, List Interval.Interval )
+        addIntervalsToVoicing ( interval1, interval2, interval3 ) ( theVoicing, intervalsUsed ) =
+            if mustBeUnique && List.any (\i -> List.member i intervalsUsed) [ interval1, interval2, interval3 ] then
+                Nothing
+
+            else
+                Just ( theVoicing interval1 interval2 interval3, [ interval1, interval2, interval3 ] ++ intervalsUsed )
+
+        triplets : List Interval.Interval -> List ( Interval.Interval, Interval.Interval, Interval.Interval )
+        triplets theList =
+            List.Extra.permutations theList
+                |> List.map
+                    (\permutation ->
+                        case permutation of
+                            a :: b :: c :: _ ->
+                                Just ( a, b, c )
+
+                            _ ->
+                                Nothing
+                    )
+                |> List.filterMap identity
+    in
+    VoicingClassBuilder
+        (List.concatMap
+            (\voicingWithIntervals ->
+                List.filterMap
+                    (\twoIntervals ->
+                        addIntervalsToVoicing twoIntervals voicingWithIntervals
+                    )
+                    (triplets options)
+            )
+            incompleteVoicingsWithIntervalsSoFar
+        )
+
+
+execute : VoicingClassBuilder a -> List a
+execute (VoicingClassBuilder voicingsWithIntervals) =
+    List.map (\( v, i ) -> v) voicingsWithIntervals
---
+++ tests/Test/VoicingClass.elm
@@ -0,0 +1,304 @@
+module Test.VoicingClass exposing (..)
+
+import Expect
+import MusicTheory.Interval as Interval
+import MusicTheory.Voicing.FourPart as FourPart
+import MusicTheory.VoicingClass as VoicingClass
+import Test exposing (Test, describe, test)
+
+
+all : Test
+all =
+    describe "Voicing Tests"
+        [ describe "building voicing classes"
+            [ describe "withFactor"
+                [ test "builds a single voicing class" <|
+                    \_ ->
+                        let
+                            expected =
+                                [ { voiceOne = Interval.perfectUnison
+                                  , voiceTwo = Interval.majorThird
+                                  , voiceThree = Interval.perfectFifth
+                                  , voiceFour = Interval.majorSeventh
+                                  }
+                                ]
+
+                            result =
+                                VoicingClass.builder FourPart.VoicingClass
+                                    |> VoicingClass.withFactor Interval.perfectUnison
+                                        { mustBeUnique = False }
+                                    |> VoicingClass.withFactor Interval.majorThird
+                                        { mustBeUnique = False }
+                                    |> VoicingClass.withFactor Interval.perfectFifth
+                                        { mustBeUnique = False }
+                                    |> VoicingClass.withFactor Interval.majorSeventh
+                                        { mustBeUnique = False }
+                                    |> VoicingClass.execute
+                        in
+                        Expect.equal expected result
+                ]
+            , describe "withFactorFrom"
+                [ test "builds multiple voicing classes from permutations of options" <|
+                    \_ ->
+                        let
+                            expected =
+                                [ { voiceOne = Interval.perfectUnison
+                                  , voiceTwo = Interval.majorThird
+                                  , voiceThree = Interval.perfectFifth
+                                  , voiceFour = Interval.minorSeventh
+                                  }
+                                , { voiceOne = Interval.perfectUnison
+                                  , voiceTwo = Interval.majorThird
+                                  , voiceThree = Interval.majorThirteenth
+                                  , voiceFour = Interval.minorSeventh
+                                  }
+                                , { voiceOne = Interval.majorNinth
+                                  , voiceTwo = Interval.majorThird
+                                  , voiceThree = Interval.perfectFifth
+                                  , voiceFour = Interval.minorSeventh
+                                  }
+                                , { voiceOne = Interval.majorNinth
+                                  , voiceTwo = Interval.majorThird
+                                  , voiceThree = Interval.majorThirteenth
+                                  , voiceFour = Interval.minorSeventh
+                                  }
+                                ]
+
+                            result =
+                                VoicingClass.builder FourPart.VoicingClass
+                                    |> VoicingClass.withFactorFrom
+                                        [ Interval.perfectUnison
+                                        , Interval.majorNinth
+                                        ]
+                                        { mustBeUnique = False }
+                                    |> VoicingClass.withFactor Interval.majorThird
+                                        { mustBeUnique = False }
+                                    |> VoicingClass.withFactorFrom
+                                        [ Interval.perfectFifth
+                                        , Interval.majorThirteenth
+                                        ]
+                                        { mustBeUnique = False }
+                                    |> VoicingClass.withFactor Interval.minorSeventh
+                                        { mustBeUnique = False }
+                                    |> VoicingClass.execute
+                        in
+                        Expect.equal expected result
+                , test "does not duplicate already used factors when mustBeUnique is True" <|
+                    \_ ->
+                        let
+                            expected =
+                                [ { voiceOne = Interval.perfectUnison
+                                  , voiceTwo = Interval.majorThird
+                                  , voiceThree = Interval.perfectFifth
+                                  , voiceFour = Interval.minorSeventh
+                                  }
+                                ]
+
+                            result =
+                                VoicingClass.builder FourPart.VoicingClass
+                                    |> VoicingClass.withFactor
+                                        Interval.perfectUnison
+                                        { mustBeUnique = False }
+                                    |> VoicingClass.withFactor
+                                        Interval.majorThird
+                                        { mustBeUnique = False }
+                                    |> VoicingClass.withFactorFrom
+                                        [ Interval.perfectFifth
+                                        , Interval.perfectUnison
+                                        ]
+                                        { mustBeUnique = True }
+                                    |> VoicingClass.withFactor
+                                        Interval.minorSeventh
+                                        { mustBeUnique = False }
+                                    |> VoicingClass.execute
+                        in
+                        Expect.equal expected result
+                , describe "withTwoFactorsFrom"
+                    [ test "builds multiple voicing classes from permutations of options" <|
+                        \_ ->
+                            let
+                                expected =
+                                    [ { voiceOne = Interval.majorNinth
+                                      , voiceTwo = Interval.majorThird
+                                      , voiceThree = Interval.majorSeventh
+                                      , voiceFour = Interval.perfectUnison
+                                      }
+                                    , { voiceOne = Interval.majorNinth
+                                      , voiceTwo = Interval.majorThird
+                                      , voiceThree = Interval.perfectFifth
+                                      , voiceFour = Interval.perfectUnison
+                                      }
+                                    , { voiceOne = Interval.majorNinth
+                                      , voiceTwo = Interval.majorSeventh
+                                      , voiceThree = Interval.majorThird
+                                      , voiceFour = Interval.perfectUnison
+                                      }
+                                    , { voiceOne = Interval.majorNinth
+                                      , voiceTwo = Interval.majorSeventh
+                                      , voiceThree = Interval.perfectFifth
+                                      , voiceFour = Interval.perfectUnison
+                                      }
+                                    , { voiceOne = Interval.majorNinth
+                                      , voiceTwo = Interval.perfectFifth
+                                      , voiceThree = Interval.majorThird
+                                      , voiceFour = Interval.perfectUnison
+                                      }
+                                    , { voiceOne = Interval.majorNinth
+                                      , voiceTwo = Interval.perfectFifth
+                                      , voiceThree = Interval.majorSeventh
+                                      , voiceFour = Interval.perfectUnison
+                                      }
+                                    ]
+
+                                result =
+                                    VoicingClass.builder FourPart.VoicingClass
+                                        |> VoicingClass.withFactor Interval.majorNinth
+                                            { mustBeUnique = False }
+                                        |> VoicingClass.withTwoFactorsFrom
+                                            [ Interval.majorThird
+                                            , Interval.majorSeventh
+                                            , Interval.perfectFifth
+                                            ]
+                                            { mustBeUnique = False }
+                                        |> VoicingClass.withFactor Interval.perfectUnison
+                                            { mustBeUnique = False }
+                                        |> VoicingClass.execute
+                            in
+                            Expect.equal expected result
+                    , test "does not duplicate already used factors when mustBeUnique is True" <|
+                        \_ ->
+                            let
+                                expected =
+                                    [ { voiceOne = Interval.majorNinth
+                                      , voiceTwo = Interval.majorThird
+                                      , voiceThree = Interval.perfectFifth
+                                      , voiceFour = Interval.perfectUnison
+                                      }
+                                    , { voiceOne = Interval.majorNinth
+                                      , voiceTwo = Interval.perfectFifth
+                                      , voiceThree = Interval.majorThird
+                                      , voiceFour = Interval.perfectUnison
+                                      }
+                                    ]
+
+                                result =
+                                    VoicingClass.builder FourPart.VoicingClass
+                                        |> VoicingClass.withFactor Interval.majorNinth
+                                            { mustBeUnique = False }
+                                        |> VoicingClass.withTwoFactorsFrom
+                                            [ Interval.majorThird
+                                            , Interval.majorNinth
+                                            , Interval.perfectFifth
+                                            ]
+                                            { mustBeUnique = True }
+                                        |> VoicingClass.withFactor Interval.perfectUnison
+                                            { mustBeUnique = False }
+                                        |> VoicingClass.execute
+                            in
+                            Expect.equal expected result
+                    ]
+                , describe "withThreeFactorsFrom"
+                    [ test "builds multiple voicing classes from permutations of options" <|
+                        \_ ->
+                            let
+                                expected =
+                                    [ { voiceOne = Interval.majorThird
+                                      , voiceTwo = Interval.perfectFifth
+                                      , voiceThree = Interval.minorSeventh
+                                      , voiceFour = Interval.perfectUnison
+                                      }
+                                    , { voiceOne = Interval.majorThird
+                                      , voiceTwo = Interval.minorSeventh
+                                      , voiceThree = Interval.perfectFifth
+                                      , voiceFour = Interval.perfectUnison
+                                      }
+                                    , { voiceOne = Interval.perfectFifth
+                                      , voiceTwo = Interval.majorThird
+                                      , voiceThree = Interval.minorSeventh
+                                      , voiceFour = Interval.perfectUnison
+                                      }
+                                    , { voiceOne = Interval.perfectFifth
+                                      , voiceTwo = Interval.minorSeventh
+                                      , voiceThree = Interval.majorThird
+                                      , voiceFour = Interval.perfectUnison
+                                      }
+                                    , { voiceOne = Interval.minorSeventh
+                                      , voiceTwo = Interval.majorThird
+                                      , voiceThree = Interval.perfectFifth
+                                      , voiceFour = Interval.perfectUnison
+                                      }
+                                    , { voiceOne = Interval.minorSeventh
+                                      , voiceTwo = Interval.perfectFifth
+                                      , voiceThree = Interval.majorThird
+                                      , voiceFour = Interval.perfectUnison
+                                      }
+                                    ]
+
+                                result : List FourPart.VoicingClass
+                                result =
+                                    VoicingClass.builder FourPart.VoicingClass
+                                        |> VoicingClass.withThreeFactorsFrom
+                                            [ Interval.majorThird
+                                            , Interval.perfectFifth
+                                            , Interval.minorSeventh
+                                            ]
+                                            { mustBeUnique = False }
+                                        |> VoicingClass.withFactor Interval.perfectUnison
+                                            { mustBeUnique = False }
+                                        |> VoicingClass.execute
+                            in
+                            Expect.equal expected result
+                    , test "does not duplicate already used factors when mustBeUnique is True" <|
+                        \_ ->
+                            let
+                                expected =
+                                    [ { voiceOne = Interval.majorNinth
+                                      , voiceTwo = Interval.majorThird
+                                      , voiceThree = Interval.perfectFifth
+                                      , voiceFour = Interval.majorThirteenth
+                                      }
+                                    , { voiceOne = Interval.majorNinth
+                                      , voiceTwo = Interval.majorThird
+                                      , voiceThree = Interval.majorThirteenth
+                                      , voiceFour = Interval.perfectFifth
+                                      }
+                                    , { voiceOne = Interval.majorNinth
+                                      , voiceTwo = Interval.perfectFifth
+                                      , voiceThree = Interval.majorThird
+                                      , voiceFour = Interval.majorThirteenth
+                                      }
+                                    , { voiceOne = Interval.majorNinth
+                                      , voiceTwo = Interval.perfectFifth
+                                      , voiceThree = Interval.majorThirteenth
+                                      , voiceFour = Interval.majorThird
+                                      }
+                                    , { voiceOne = Interval.majorNinth
+                                      , voiceTwo = Interval.majorThirteenth
+                                      , voiceThree = Interval.majorThird
+                                      , voiceFour = Interval.perfectFifth
+                                      }
+                                    , { voiceOne = Interval.majorNinth
+                                      , voiceTwo = Interval.majorThirteenth
+                                      , voiceThree = Interval.perfectFifth
+                                      , voiceFour = Interval.majorThird
+                                      }
+                                    ]
+
+                                result =
+                                    VoicingClass.builder FourPart.VoicingClass
+                                        |> VoicingClass.withFactor Interval.majorNinth
+                                            { mustBeUnique = False }
+                                        |> VoicingClass.withThreeFactorsFrom
+                                            [ Interval.majorThird
+                                            , Interval.majorNinth
+                                            , Interval.perfectFifth
+                                            , Interval.majorThirteenth
+                                            ]
+                                            { mustBeUnique = True }
+                                        |> VoicingClass.execute
+                            in
+                            Expect.equal expected result
+                    ]
+                ]
+            ]
+        ]
