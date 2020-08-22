3a89c11478aaa1c767cd3074d8c57731bbcb858f
Duncan Malashock <duncanmalashock@gmail.com>
2020-08-22 13:21:06 -0400

    Add Interval docs

--- src/MusicTheory/Interval.elm
+++ src/MusicTheory/Interval.elm
@@ -1,46 +1,58 @@
 module MusicTheory.Interval exposing
     ( Interval
     , perfectUnison, minorSecond, majorSecond, minorThird, majorThird, perfectFourth, perfectFifth, minorSixth, majorSixth, minorSeventh, majorSeventh, perfectOctave
-    , augmentedUnison, augmentedSecond, augmentedThird, augmentedFourth, augmentedFifth, augmentedSixth, augmentedSeventh, augmentedOctave
+    , augmentedUnison, augmentedSecond, augmentedThird, augmentedFourth, augmentedFifth, augmentedSixth, augmentedSeventh
     , diminishedSecond, diminishedThird, diminishedFourth, diminishedFifth, diminishedSixth, diminishedSeventh, diminishedOctave
     , minorNinth, majorNinth, minorTenth, majorTenth, perfectEleventh, perfectTwelfth, minorThirteenth, majorThirteenth
-    , augmentedNinth, augmentedEleventh, augmentedTwelfth
+    , augmentedOctave, augmentedNinth, augmentedEleventh, augmentedTwelfth
     , diminishedTwelfth
     )
 
-{-|
+{-| An interval is a measure of the distance between two pitches.
 
 @docs Interval
 
 
 # Interval constructors
 
+Note: if you're not familiar with the way intervals are classified, here's some explanation of the terminology here. Intervals can be:
+
+**Simple or compound**:
+
+[Simple intervals](https://en.wikipedia.org/wiki/Interval_%28music%29#Simple_and_compound) span a distance of an octave or less, and [compound intervals](https://en.wikipedia.org/wiki/Interval_%28music%29#Simple_and_compound) span a distance of more than one octave.
+
+**Diatonic or chromatic**:
+
+[Diatonic intervals](https://en.wikipedia.org/wiki/Interval_%28music%29#Diatonic_and_chromatic) are those found in diatonic scales (major, minor, and perfect intervals), and [chromatic intervals](https://en.wikipedia.org/wiki/Interval_%28music%29#Diatonic_and_chromatic) are those that aren't (augmented and diminished intervals).
+
 
 ## Simple intervals
 
 
-### Diatonic
+### Simple diatonic
 
 @docs perfectUnison, minorSecond, majorSecond, minorThird, majorThird, perfectFourth, perfectFifth, minorSixth, majorSixth, minorSeventh, majorSeventh, perfectOctave
 
 
-### Chromatic
+### Simple chromatic
 
-@docs augmentedUnison, augmentedSecond, augmentedThird, augmentedFourth, augmentedFifth, augmentedSixth, augmentedSeventh, augmentedOctave
+@docs augmentedUnison, augmentedSecond, augmentedThird, augmentedFourth, augmentedFifth, augmentedSixth, augmentedSeventh
 @docs diminishedSecond, diminishedThird, diminishedFourth, diminishedFifth, diminishedSixth, diminishedSeventh, diminishedOctave
 
 
 ## Compound intervals
 
+Note: This module includes constructors for compound intervals up to the thirteenth, which is the highest interval found in standard chords in tonal music. If you need larger compound intervals, take a look at the `add` or `addOctave` helper functions in this module.
 
-### Diatonic
+
+### Compound diatonic
 
 @docs minorNinth, majorNinth, minorTenth, majorTenth, perfectEleventh, perfectTwelfth, minorThirteenth, majorThirteenth
 
 
-### Chromatic
+### Compound chromatic
 
-@docs augmentedNinth, augmentedEleventh, augmentedTwelfth
+@docs augmentedOctave, augmentedNinth, augmentedEleventh, augmentedTwelfth
 @docs diminishedTwelfth
 
 -}
