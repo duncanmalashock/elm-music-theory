9b6df4e11c7c263353f7113a964846ddb867c8e2
Duncan Malashock <duncanmalashock@gmail.com>
2020-09-02 11:07:55 -0400

    Update docs

--- src/Music/Chord/Classification.elm
+++ src/Music/Chord/Classification.elm
@@ -1,6 +1,20 @@
 module Music.Chord.Classification exposing (Classification(..), Triad(..), SixthOrSeventh(..), UnalteredExtension(..), AlteredExtension(..))
 
-{-| This module contains a `Classification` type you can use to make custom chord symbols. You probably won't need it, but I have included it to provide more options than `ChordType.symbol` offers.
+{-| This module contains a `Classification` type you can use to make custom chord symbols. If `ChordType.symbol` works for your purposes, then you don't need this. But I have included it to provide more options than `ChordType.symbol` offers.
+
+
+# Disclaimer
+
+
+## Classifying chords exhaustively is hard and weird!
+
+Different chord classification methods have accumulated since at least the 17th century, and they mainly serve to describe the most usual cases in a particular idiom. To my knowledge there is no unified method for classifying chord types into a taxonomy that is also used by practicing musicians.
+
+This makes classifying chords a complicated topic, because some categories that are normally isolated from each other can potentially interact in unspecified ways.
+
+For instance, non-tertian "added-tone" chords and "altered extensions" on 7th chords belong to separate models of chord categorization. Can added-tone chords like m6 have extensions, as in a m6(♭13)? I have personally never seen such a chord, but I have also never seen a rule that says why it cannot be done!
+
+This `Classification` type is my best attempt to codify the taxonomy I'm aware of into a type you can destructure for your own purposes. Just be aware that, for the reasons I've described, it can represent some combinations that are unusual but not clearly invalid. Use your judgment as to which of these you want to handle, and which you want to ignore.
 
 @docs Classification, Triad, SixthOrSeventh, UnalteredExtension, AlteredExtension
 
