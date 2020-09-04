6cfd21444fe1e07c499b74a985be0365b0bbc9a0
Duncan Malashock <duncanmalashock@gmail.com>
2020-09-04 10:09:23 -0400

    Update Classification.elm

--- src/Music/Chord/Classification.elm
+++ src/Music/Chord/Classification.elm
@@ -6,15 +6,15 @@ module Music.Chord.Classification exposing (Classification(..), Triad(..), Sixth
 # Disclaimer
 
 
-## Classifying chords exhaustively is hard and weird!
+## Classifying chords exhaustively is hard! I did my best
 
-Different chord classification methods have accumulated since at least the 17th century, and they mainly serve to describe the most usual cases in a particular idiom. To my knowledge there is no unified method for classifying chord types into a taxonomy that is also used by practicing musicians.
+Different chord classification methods have accumulated since at least the 17th century, and they mainly serve to describe the most usual cases in a particular idiom. To my knowledge, there is no unified method for classifying chord types into a taxonomy that is also used by practicing musicians.
 
-This makes classifying chords a complicated topic, because some categories that are normally isolated from each other can potentially interact in unspecified ways.
+This makes classifying chords a complicated topic. Edge cases are everywhere, because some categories that are normally isolated from each other, with only occasional combination, can potentially interact in unspecified ways.
 
-For instance, non-tertian "added-tone" chords and "altered extensions" on 7th chords belong to separate models of chord categorization. Can added-tone chords like m6 have extensions, as in a m6(♭13)? I have personally never seen such a chord, but I have also never seen a rule that says why it cannot be done!
+For instance, non-tertian "added-tone" chords and "altered extensions" on 7th chords belong to somewhat separate models of chord categorization, but 6th chords can have 9ths. Does this mean added-tone chords like m6 can have any extensions, as in a m6(♯11)? I have personally never seen any such chord, but I have also never seen a rule that says why it cannot be done!
 
-This `Classification` type is my best attempt to codify the taxonomy I'm aware of into a type you can destructure for your own purposes. Just be aware that, for the reasons I've described, it can represent some combinations that are unusual but not clearly invalid. Use your judgment as to which of these you want to handle, and which you want to ignore.
+This `Classification` type is my best attempt to codify the chord type taxonomy, as I understand it, into a type you can destructure for your own purposes (i.e. custom views of chord symbols). Just be aware that, for the reasons I've described, it can represent some combinations that are unusual but not clearly invalid. Use your judgment as to which of these you want to handle, and which you want to ignore.
 
 @docs Classification, Triad, SixthOrSeventh, UnalteredExtension, AlteredExtension
 
