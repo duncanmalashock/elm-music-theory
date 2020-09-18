b1411594032b5b4bf24b24f605dab2b4497e01f2
Duncan Malashock <duncanmalashock@gmail.com>
2020-09-18 08:45:13 -0400

    Update Main.elm

--- examples/lead-sheet/src/Main.elm
+++ examples/lead-sheet/src/Main.elm
@@ -3,7 +3,6 @@ module Main exposing (main)
 import Browser
 import Element
 import Element.Background
-import Element.Events
 import Element.Font
 import Element.Input
 import Html exposing (Html)
@@ -15,11 +14,7 @@ import Music.Key as Key exposing (Key)
 import Music.PitchClass as PitchClass
 
 
-type alias Flags =
-    ()
-
-
-main : Program Flags Model Msg
+main : Program () Model Msg
 main =
     Browser.document
         { init = init
@@ -27,6 +22,86 @@ main =
         , update = update
         , subscriptions = always Sub.none
         }
+
+
+
+-- Model & Init
+
+
+type alias Model =
+    { measures : List Measure
+    , currentKey : Key
+    , analyzed : Bool
+    }
+
+
+init : () -> ( Model, Cmd msg )
+init flags =
+    ( { currentKey = Key.bFlat
+      , analyzed = False
+      , measures = initialMeasures
+      }
+    , Cmd.none
+    )
+
+
+initialMeasures : List Measure
+initialMeasures =
+    [ measure2Chords (Chord.majorSeventh PitchClass.bFlat) (Chord.minorSeventh PitchClass.c)
+    , measure2Chords (Chord.minorSeventh PitchClass.d) (Chord.diminishedSeventh PitchClass.dFlat)
+    , measure2Chords (Chord.minorSeventh PitchClass.c) (Chord.dominantSeventh PitchClass.f)
+    , measure2Chords (Chord.majorSeventh PitchClass.bFlat) (Chord.dominantSeventh PitchClass.d)
+
+    --
+    , measure2Chords (Chord.minorSeventh PitchClass.g) (Chord.dominantSeventh PitchClass.d)
+    , measure2Chords (Chord.minorSeventh PitchClass.g) (Chord.dominantSeventh PitchClass.g)
+    , measure2Chords (Chord.minorSeventh PitchClass.c) (Chord.dominantSeventh PitchClass.f)
+    , measure2Chords (Chord.majorSeventh PitchClass.bFlat) (Chord.dominantSeventh PitchClass.bFlat)
+
+    --
+    , measure2Chords (Chord.majorSeventh PitchClass.eFlat) (Chord.dominantSeventh PitchClass.aFlat)
+    , measure2Chords (Chord.majorSeventh PitchClass.bFlat) (Chord.dominantSeventh PitchClass.bFlat)
+    , measure2Chords (Chord.majorSeventh PitchClass.eFlat) (Chord.dominantSeventh PitchClass.aFlat)
+    , measure1Chord (Chord.majorSeventh PitchClass.bFlat)
+
+    --
+    , measure2Chords (Chord.halfDiminished PitchClass.e) (Chord.dominantSeventhFlatNine PitchClass.a)
+    , measure2Chords (Chord.minorSeventh PitchClass.d) (Chord.dominantSeventh PitchClass.dFlat)
+    , measure2Chords (Chord.dominantSeventhSus4 PitchClass.c) (Chord.dominantSeventh PitchClass.c)
+    , measure2Chords (Chord.minorSeventh PitchClass.c) (Chord.dominantSeventh PitchClass.f)
+
+    --
+    , measure2Chords (Chord.majorSeventh PitchClass.bFlat) (Chord.minorSeventh PitchClass.c)
+    , measure2Chords (Chord.minorSeventh PitchClass.d) (Chord.diminishedSeventh PitchClass.dFlat)
+    , measure2Chords (Chord.minorSeventh PitchClass.c) (Chord.dominantSeventh PitchClass.f)
+    , measure2Chords (Chord.majorSeventh PitchClass.bFlat) (Chord.dominantSeventh PitchClass.d)
+
+    --
+    , measure2Chords (Chord.minorSeventh PitchClass.g) (Chord.dominantSeventh PitchClass.d)
+    , measure2Chords (Chord.minorSeventh PitchClass.g) (Chord.dominantSeventh PitchClass.g)
+    , measure2Chords (Chord.minorSeventh PitchClass.c) (Chord.dominantSeventh PitchClass.f)
+    , measure2Chords (Chord.majorSeventh PitchClass.bFlat) (Chord.dominantSeventh PitchClass.bFlat)
+
+    --
+    , measure2Chords (Chord.majorSeventh PitchClass.eFlat) (Chord.dominantSeventh PitchClass.g)
+    , measure1Chord (Chord.minorSeventh PitchClass.c)
+    , measure2Chords (Chord.halfDiminished PitchClass.a) (Chord.dominantSeventhFlatNine PitchClass.d)
+    , measure2Chords (Chord.minorSeventh PitchClass.g) (Chord.dominantSeventh PitchClass.gFlat)
+
+    --
+    , measure1Chord (Chord.majorSeventh PitchClass.bFlat)
+    , measure2Chords (Chord.minorSeventh PitchClass.c) (Chord.dominantSeventh PitchClass.f)
+    , measure1Chord (Chord.majorSix PitchClass.bFlat)
+    , measure2Chords (Chord.minorSeventh PitchClass.c) (Chord.dominantSeventh PitchClass.f)
+    ]
+
+
+
+--  Measures & Entries
+
+
+type Measure
+    = Measure (Maybe Entry) (Maybe Entry)
 
 
 measure1Chord : Chord.Chord -> Measure
@@ -39,75 +114,15 @@ measure2Chords first second =
     Measure (initEntry first) (initEntry second)
 
 
-measureEmpty : Measure
-measureEmpty =
-    Measure Nothing Nothing
-
-
 mapMeasure : (Entry -> Entry) -> Measure -> Measure
 mapMeasure fn (Measure first second) =
     Measure (Maybe.map fn first) (Maybe.map fn second)
 
 
-type Measure
-    = Measure (Maybe Entry) (Maybe Entry)
-
-
-init : Flags -> ( Model, Cmd msg )
-init flags =
-    ( { currentKey = Key.bFlat
-      , analyzed = False
-      , measures =
-            [ measure2Chords (Chord.majorSeventh PitchClass.bFlat) (Chord.minorSeventh PitchClass.c)
-            , measure2Chords (Chord.minorSeventh PitchClass.d) (Chord.diminishedSeventh PitchClass.dFlat)
-            , measure2Chords (Chord.minorSeventh PitchClass.c) (Chord.dominantSeventh PitchClass.f)
-            , measure2Chords (Chord.majorSeventh PitchClass.bFlat) (Chord.dominantSeventh PitchClass.d)
-
-            --
-            , measure2Chords (Chord.minorSeventh PitchClass.g) (Chord.dominantSeventh PitchClass.d)
-            , measure2Chords (Chord.minorSeventh PitchClass.g) (Chord.dominantSeventh PitchClass.g)
-            , measure2Chords (Chord.minorSeventh PitchClass.c) (Chord.dominantSeventh PitchClass.f)
-            , measure2Chords (Chord.majorSeventh PitchClass.bFlat) (Chord.dominantSeventh PitchClass.bFlat)
-
-            --
-            , measure2Chords (Chord.majorSeventh PitchClass.eFlat) (Chord.dominantSeventh PitchClass.aFlat)
-            , measure2Chords (Chord.majorSeventh PitchClass.bFlat) (Chord.dominantSeventh PitchClass.bFlat)
-            , measure2Chords (Chord.majorSeventh PitchClass.eFlat) (Chord.dominantSeventh PitchClass.aFlat)
-            , measure1Chord (Chord.majorSeventh PitchClass.bFlat)
-
-            --
-            , measure2Chords (Chord.halfDiminished PitchClass.e) (Chord.dominantSeventhFlatNine PitchClass.a)
-            , measure2Chords (Chord.minorSeventh PitchClass.d) (Chord.dominantSeventh PitchClass.dFlat)
-            , measure2Chords (Chord.dominantSeventhSus4 PitchClass.c) (Chord.dominantSeventh PitchClass.c)
-            , measure2Chords (Chord.minorSeventh PitchClass.c) (Chord.dominantSeventh PitchClass.f)
-
-            --
-            , measure2Chords (Chord.majorSeventh PitchClass.bFlat) (Chord.minorSeventh PitchClass.c)
-            , measure2Chords (Chord.minorSeventh PitchClass.d) (Chord.diminishedSeventh PitchClass.dFlat)
-            , measure2Chords (Chord.minorSeventh PitchClass.c) (Chord.dominantSeventh PitchClass.f)
-            , measure2Chords (Chord.majorSeventh PitchClass.bFlat) (Chord.dominantSeventh PitchClass.d)
-
-            --
-            , measure2Chords (Chord.minorSeventh PitchClass.g) (Chord.dominantSeventh PitchClass.d)
-            , measure2Chords (Chord.minorSeventh PitchClass.g) (Chord.dominantSeventh PitchClass.g)
-            , measure2Chords (Chord.minorSeventh PitchClass.c) (Chord.dominantSeventh PitchClass.f)
-            , measure2Chords (Chord.majorSeventh PitchClass.bFlat) (Chord.dominantSeventh PitchClass.bFlat)
-
-            --
-            , measure2Chords (Chord.majorSeventh PitchClass.eFlat) (Chord.dominantSeventh PitchClass.g)
-            , measure1Chord (Chord.minorSeventh PitchClass.c)
-            , measure2Chords (Chord.halfDiminished PitchClass.a) (Chord.dominantSeventhFlatNine PitchClass.d)
-            , measure2Chords (Chord.minorSeventh PitchClass.g) (Chord.dominantSeventh PitchClass.gFlat)
-
-            --
-            , measure1Chord (Chord.majorSeventh PitchClass.bFlat)
-            , measure2Chords (Chord.minorSeventh PitchClass.c) (Chord.dominantSeventh PitchClass.f)
-            , measure1Chord (Chord.majorSix PitchClass.bFlat)
-            , measure2Chords (Chord.minorSeventh PitchClass.c) (Chord.dominantSeventh PitchClass.f)
-            ]
-      }
-    , Cmd.none
-    )
+type alias Entry =
+    { analysis : Maybe Analysis
+    , chord : Chord
+    }
 
 
 initEntry : Chord -> Maybe Entry
@@ -118,69 +133,11 @@ initEntry chord =
         }
 
 
-type alias Model =
-    { measures : List Measure
-    , currentKey : Key
-    , analyzed : Bool
-    }
-
-
-type alias Entry =
-    { analysis : Maybe Analysis
-    , chord : Chord
-    }
-
-
-type Row
-    = Row (Maybe Measure) (Maybe Measure) (Maybe Measure) (Maybe Measure)
-
-
-type Msg
-    = AnalyzeClicked
-    | NewKeyChosen Key
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        AnalyzeClicked ->
-            ( analyzeMeasures model, Cmd.none )
-
-        NewKeyChosen key ->
-            ( transposeEntries key model, Cmd.none )
-
-
-analyzeMeasures : Model -> Model
-analyzeMeasures model =
-    { model
-        | measures =
-            List.map
-                (mapMeasure
-                    (analyzeEntry model.currentKey)
-                )
-                model.measures
-        , analyzed = True
-    }
-
-
 analyzeEntry : Key -> Entry -> Entry
 analyzeEntry currentKey entry =
     { entry
         | analysis =
             Just (Analysis.analyze entry.chord currentKey)
-    }
-
-
-transposeEntries : Key -> Model -> Model
-transposeEntries newKey model =
-    { model
-        | currentKey = newKey
-        , measures =
-            List.map
-                (mapMeasure
-                    (transposeEntry newKey)
-                )
-                model.measures
     }
 
 
@@ -197,6 +154,49 @@ transposeEntry currentKey entry =
     }
 
 
+
+-- Msg and Update
+
+
+type Msg
+    = AnalyzeClicked
+    | NewKeyChosen Key
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        AnalyzeClicked ->
+            ( { model
+                | measures =
+                    List.map
+                        (mapMeasure
+                            (analyzeEntry model.currentKey)
+                        )
+                        model.measures
+                , analyzed = True
+              }
+            , Cmd.none
+            )
+
+        NewKeyChosen newKey ->
+            ( { model
+                | currentKey = newKey
+                , measures =
+                    List.map
+                        (mapMeasure
+                            (transposeEntry newKey)
+                        )
+                        model.measures
+              }
+            , Cmd.none
+            )
+
+
+
+-- View
+
+
 view : Model -> { title : String, body : List (Html Msg) }
 view model =
     { title = "Lead Sheet"
@@ -207,6 +207,10 @@ view model =
             (viewBody model)
         ]
     }
+
+
+type Row
+    = Row (Maybe Measure) (Maybe Measure) (Maybe Measure) (Maybe Measure)
 
 
 viewBody : Model -> Element.Element Msg
@@ -239,7 +243,7 @@ viewBody model =
         ]
         [ Element.column
             [ Element.width Element.fill
-            , Element.spacing 20
+            , Element.spacing 40
             ]
             (List.map (viewRow model.currentKey) rows)
         , Element.Input.button
@@ -267,42 +271,6 @@ viewRow currentKey (Row a b c d) =
         , maybeViewMeasure c
         , maybeViewMeasure d
         ]
-
-
-
---, viewTransposeControls model
-
-
-viewTransposeControls : Model -> Html Msg
-viewTransposeControls model =
-    let
-        keyMenuItems =
-            [ Key.c
-            , Key.dFlat
-            , Key.d
-            , Key.eFlat
-            , Key.e
-            , Key.f
-            , Key.fSharp
-            , Key.g
-            , Key.aFlat
-            , Key.a
-            , Key.bFlat
-            , Key.b
-            ]
-                |> List.map
-                    (\key ->
-                        Html.option [ Html.Events.onClick (NewKeyChosen key) ]
-                            [ Html.text (Key.toString key)
-                            ]
-                    )
-    in
-    if model.analyzed then
-        Html.select []
-            keyMenuItems
-
-    else
-        Html.text ""
 
 
 viewMeasure : Key -> Measure -> Element.Element Msg
@@ -342,6 +310,7 @@ viewMaybeEntry currentKey maybeEntry =
         Just entry ->
             Element.column
                 [ Element.Font.size 26
+                , Element.Font.bold
                 , Element.width Element.fill
                 , Element.spacing 18
                 ]
@@ -359,9 +328,42 @@ viewAnalysis currentKey maybeAnalysis =
         Just analysis ->
             Element.el
                 [ Element.Font.size 20
+                , Element.Font.bold
                 , Element.Font.color (Element.rgb 0.5 0.5 0.5)
                 ]
                 (Element.text (Analysis.toString currentKey analysis))
 
         Nothing ->
             Element.none
+
+
+viewTransposeControls : Model -> Html Msg
+viewTransposeControls model =
+    let
+        keyMenuItems =
+            [ Key.c
+            , Key.dFlat
+            , Key.d
+            , Key.eFlat
+            , Key.e
+            , Key.f
+            , Key.fSharp
+            , Key.g
+            , Key.aFlat
+            , Key.a
+            , Key.bFlat
+            , Key.b
+            ]
+                |> List.map
+                    (\key ->
+                        Html.option [ Html.Events.onClick (NewKeyChosen key) ]
+                            [ Html.text (Key.toString key)
+                            ]
+                    )
+    in
+    if model.analyzed then
+        Html.select []
+            keyMenuItems
+
+    else
+        Html.text ""
