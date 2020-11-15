4d3c4420347af4eb781627e623854ff0379b8ed2
Duncan Malashock <duncanmalashock@gmail.com>
2020-11-15 18:38:23 -0500

    Remove previous implementations of melody, harmonize modules

--- src/Music/Internal/HarmonicContext.elm
+++ src/Music/Internal/HarmonicContext.elm
@@ -2,14 +2,10 @@ module Music.Internal.HarmonicContext exposing
     ( HarmonicContext
     , chord
     , init
-    , pitch
-    , pitchClass
     , scale
     )
 
 import Music.Internal.Chord as Chord
-import Music.Internal.Pitch as Pitch
-import Music.Internal.PitchClass as PitchClass
 import Music.Internal.Scale as Scale
 
 
@@ -18,27 +14,15 @@ type HarmonicContext
 
 
 type alias HarmonicContextInput =
-    { pitch : Pitch.Pitch
-    , chord : Chord.Chord
+    { chord : Chord.Chord
     , scale : Scale.Scale
     }
 
 
 init : HarmonicContextInput -> HarmonicContext
 init input =
-    HarmonicContextInput input.pitch input.chord input.scale
+    HarmonicContextInput input.chord input.scale
         |> HarmonicContext
-
-
-pitch : HarmonicContext -> Pitch.Pitch
-pitch (HarmonicContext contextData) =
-    contextData.pitch
-
-
-pitchClass : HarmonicContext -> PitchClass.PitchClass
-pitchClass (HarmonicContext contextData) =
-    contextData.pitch
-        |> Pitch.pitchClass
 
 
 chord : HarmonicContext -> Chord.Chord
